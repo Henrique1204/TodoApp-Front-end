a9589b7cacde714318929ba9151a4f0f132f529e
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 17:44:44 -0300

    Preencher componente About

--- src/about/About.jsx
+++ src/about/About.jsx
@@ -6,6 +6,15 @@ const About = () => {
     return (
         <div>
             <PageHeader name="Sobre" small="Nós" />
+
+            <h2>Nossa História</h2>
+            <p>Lorem impsum dolor it amet...</p>
+
+            <h2>Missão e Visão</h2>
+            <p>Lorem impsum dolor it amet...</p>
+
+            <h2>Imprensa</h2>
+            <p>Lorem impsum dolor it amet...</p>
         </div>
     );
 };
