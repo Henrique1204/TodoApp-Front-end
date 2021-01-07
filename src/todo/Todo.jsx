f189355ee3d3d62ffd44401d7c2e2c0df6f4a1ab
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 20:36:22 -0300

    Implementar requisicao POST para API

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -4,12 +4,22 @@ import PageHeader from "../template/PageHeader.jsx";
 import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList.jsx";
 
+const URL = "http://localhost:3003/api/todo";
+
 const Todo = () => {
     const [description, setDescription] = React.useState("");
     const [list, setLista] = React.useState([]);
 
-    const handleAdd = () => {
-        console.log(description);
+    const handleAdd = async () => {
+        const res = await fetch(URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ description })
+        });
+
+        const json = await res.json();
     };
 
     const handleChange = ({ target }) => setDescription(target.value);
