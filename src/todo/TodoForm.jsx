15fd5d152d2ed8b943560ed909e09ad378056b98
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 18:09:42 -0300

    Adicionar estrutura do TodoForm

--- src/todo/TodoForm.jsx
+++ src/todo/TodoForm.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const TodoForm = () => {
     return (
-        <div>
-            <h1>Form</h1>
+        <div className="TodoForm row">
+            <div className="col-xs-12 col-sm-9 col-md-10">
+                <input type="text" className="form-control" placeholder="Adicione uma tarefa" />
+            </div>
+
+            <div className="col-xs-12 col-sm-3 col-md-2">
+                <button className="btn btn-primary">
+                    <i className="fa fa-plus"></i>
+                </button>
+            </div>
         </div>
     );
 };
