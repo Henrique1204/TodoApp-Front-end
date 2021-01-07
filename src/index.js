4cabc9c2bdadbad1ea33dce9a3be1688a226544d
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 16:50:58 -0300

    Iniciar projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './main/App.jsx';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
