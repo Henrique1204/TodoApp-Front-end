d770727ab76e37f7f8b4a0ac3dbe097b84218205
Paulo <pauloh16.pdr@gmail.com>
2021-01-07 17:50:51 -0300

    Criar componentes TodoForm e TodoList

--- src/todo/Todo.jsx
+++ src/todo/Todo.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 // Importando compoenentes da interface.
 import PageHeader from "../template/PageHeader.jsx";
+import TodoForm from "./TodoForm.jsx";
+import TodoList from "./TodoList.jsx";
 
 const Todo = () => {
     return (
         <div>
             <PageHeader name="Tarefas" small="Cadastro" />
+            <TodoForm />
+            <TodoList />
         </div>
     );
 };
