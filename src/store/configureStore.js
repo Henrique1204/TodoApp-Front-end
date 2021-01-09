570bd7d1690126254de5aab362981397668442dc
Paulo <pauloh16.pdr@gmail.com>
2021-01-09 20:24:23 -0300

    Configurar store inicial

---
+++ src/store/configureStore.js
@@ -0,0 +1,29 @@
+// Importando utilitários da toolkit do Redux.
+import { configureStore, combineReducers, getDefaultMiddleware } from "@reduxjs/toolkit";
+
+// Definindo os middlewares mantendo o que vem por padrão.
+const middleware = [...getDefaultMiddleware()];
+// Combinando reducers para criar o reducer da aplicação.
+const reducer = combineReducers({
+    todo: {
+        description: "Ler livro",
+        list: [{
+            _id: 1,
+            description: "Pagar fatura do cartão.",
+            done: true
+        }, {
+            _id: 2,
+            description: "Reunião com a equipe às 10:00h",
+            done: false
+        }, {
+            _id: 3,
+            description: "Consulta médica na terça depois do almoço.",
+            done: false
+        }]
+    }
+});
+
+// Criando a store passando o reducer e os middlewares.
+const store = configureStore(reducer, middleware);
+
+export default store;
